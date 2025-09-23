5a03456fdb2b8d64f03f6c7ca1ed54d543cfc193
Ainsley <parkhua@mail.nmc.edu>
2025-09-23 13:34:28 -0400

    Initial commit

---
+++ app/src/main/java/com/zybooks/tictactoe/GameModel.java
@@ -0,0 +1,56 @@
+package com.zybooks.tictactoe;
+
+public class GameModel {
+    private char[] board;
+    private char currentPlayer;
+
+    public GameModel() {
+        board = new char[9];
+        reset();
+    }
+
+    public char getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public boolean makeMove(int index) {
+        if (board[index] == ' ') {
+            board[index] = currentPlayer;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean checkWin() {
+        int[][] wins = {
+                {0,1,2},{3,4,5},{6,7,8}, // rows
+                {0,3,6},{1,4,7},{2,5,8}, // cols
+                {0,4,8},{2,4,6}           // diagonals
+        };
+        for (int[] pos : wins) {
+            if (board[pos[0]] != ' ' &&
+                    board[pos[0]] == board[pos[1]] &&
+                    board[pos[1]] == board[pos[2]]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isBoardFull() {
+        for (char c : board) {
+            if (c == ' ') return false;
+        }
+        return true;
+    }
+
+    public void switchPlayer() {
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+    }
+
+    public void reset() {
+        for (int i = 0; i < 9; i++) board[i] = ' ';
+        currentPlayer = 'X';
+    }
+}
+
---
+++ app/src/main/java/com/zybooks/tictactoe/MainActivity.java
@@ -0,0 +1,113 @@
+package com.zybooks.tictactoe;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private GameModel game;
+    private Button[] buttons = new Button[9];
+    private TextView turnTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        // Keep Android Studio's template code
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Setup
+        game = new GameModel();
+        turnTextView = findViewById(R.id.turnTextView);
+
+        // Link 9 buttons
+        for (int i = 0; i < 9; i++) {
+            int resID = getResources().getIdentifier("button" + i, "id", getPackageName());
+            buttons[i] = findViewById(resID);
+            final int index = i;
+            buttons[i].setOnClickListener(v -> handleMove(index));
+        }
+
+        resetBoardUI();
+    }
+
+    private void handleMove(int index) {
+        if (game.makeMove(index)) {
+            buttons[index].setText(String.valueOf(game.getCurrentPlayer()));
+
+            if (game.checkWin()) {
+                Toast.makeText(this, game.getCurrentPlayer() + " Wins!", Toast.LENGTH_LONG).show();
+                disableBoard();
+                return;
+            }
+
+            if (game.isBoardFull()) {
+                Toast.makeText(this, "CAT Game!", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            game.switchPlayer();
+            turnTextView.setText(game.getCurrentPlayer() + "'s Turn");
+        }
+    }
+
+    private void disableBoard() {
+        for (Button b : buttons) b.setEnabled(false);
+    }
+
+    private void resetBoardUI() {
+        game.reset();
+        for (Button b : buttons) {
+            b.setText("");
+            b.setEnabled(true);
+        }
+        turnTextView.setText("X's Turn");
+    }
+
+    // Menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_new_game) {
+            resetBoardUI();
+            return true;
+        } else if (item.getItemId() == R.id.menu_quit) {
+            showQuitDialog();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showQuitDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("Quit")
+                .setMessage("Are you sure you want to quit?")
+                .setPositiveButton("Yes", (dialog, which) -> finish())
+                .setNegativeButton("No", null)
+                .show();
+    }
+}
